d956116dc5e6edeffbf9f945648922046ac88fd0
Andrey Verigin <andrei-verigin@mail.ru>
2017-09-02 17:19:41 +0300

    Изменение размеров поля клавишами со стрелками + много мелких исправлений

--- src/Game.hs
+++ src/Game.hs
@@ -18,7 +18,7 @@ cellSize = 30
 
 -- | Состояние поля в начале игры
 initField :: Field
-initField = Field [] 15 15 False
+initField = Field [] 15 15 True
 
 -- | Клетка поля
 type Cell = (Int, Int)
@@ -44,8 +44,8 @@ showAlive cell = uncurry translate (cellToScreen cell) (color black (rectangleSo
 
 -- | Отобразить, стоит ли игра на паузе
 showPause :: Bool -> Picture
-showPause True = color red (text "Pause")
-showPause False = color red (text "Play")
+showPause True = scale 0.5 0.5 (color red (text "Pause"))
+showPause False = Blank
 
 -- | Преобразовать координаты на игровом поле в координаты в окне
 cellToScreen :: Cell -> Point
@@ -63,15 +63,24 @@ applyRules = id
 
 -- | Обработчик событий
 handler :: Event -> Field -> Field
-handler (EventKey (MouseButton LeftButton) Down _ mouse) (Field l x y p) = Field (addCell (screenToCell mouse) l) x y p
+handler (EventKey (MouseButton LeftButton) Down _ mouse) field = addAlive (screenToCell mouse) field
 handler (EventKey (SpecialKey KeySpace) Down _ _) (Field l x y p) = Field l x y (not p)
+handler (EventKey (SpecialKey KeyUp) Down _ _) (Field l x y p) = Field l x (y + 1) p
+handler (EventKey (SpecialKey KeyDown) Down _ _) (Field l x y p)
+  | y - 1 > 0 = Field (filter (\c -> snd c >= 0 && snd c < y - 1) l) x (y - 1) p
+  | otherwise = Field l x y p
+handler (EventKey (SpecialKey KeyRight) Down _ _) (Field l x y p) = Field l (x + 1) y p
+handler (EventKey (SpecialKey KeyLeft) Down _ _) (Field l x y p)
+  | x - 1 > 0 = Field (filter (\c -> fst c >= 0 && fst c < x - 1) l) (x - 1) y p 
+  | otherwise = Field l x y p
 handler _ field = field
 
--- | Добавление клетки в список с проверкой на наличие дубликата
-addCell :: Cell -> [Cell] -> [Cell]
-addCell cell l
-  | cell `elem` l = l
-  | otherwise = cell : l
+-- | Добавление живой клетки на поле
+addAlive :: Cell -> Field -> Field
+addAlive (x, y) field
+  | x >= 0 && x < width field && y >= 0 && y < height field && not ((x, y) `elem` alive field) = 
+    Field ((x, y) : alive field) (width field) (height field) (pause field)
+  | otherwise = field
 
 -- | Преобразовать координаты в окне в координаты на игровом поле 
 screenToCell :: Point -> Cell
@@ -82,7 +91,7 @@ startGame :: Field -> IO ()
 startGame field = play window background fps field renderer handler oneStep
   where
     window = InWindow "Good Window" (windowWidth, windowHeight) (200, 200)
-    windowWidth = (round . fst . cellToScreen) (width initField, height initField) + 20
-    windowHeight = (round . snd . cellToScreen) (width initField, height initField) + 20
+    windowWidth = (round . fst . cellToScreen) (width initField, height initField) + round (2 * cellSize)
+    windowHeight = (round . snd . cellToScreen) (width initField, height initField) + round (2 * cellSize)
     background = white
     fps = 1
